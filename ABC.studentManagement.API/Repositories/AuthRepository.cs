d19b75915f5be1c19b6a63ce8d0c22fab6b73803
kusham <kusham1998@gmail.com>
2024-03-30 02:40:30 +0530

    Reapply "create repos"
    This reverts commit 6605930521be016aef7a50915c8198ea42edd822.

---
+++ ABC.studentManagement.API/Repositories/AuthRepository.cs
@@ -0,0 +1,37 @@
+﻿using ABC.studentManagement.API.Data;
+using ABC.studentManagement.API.IRepositories;
+using ABC.studentManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ABC.studentManagement.API.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public AuthRepository(ApplicationDbContext context)
+        {
+            this._dbContext = context;
+        }
+        public async Task AddStaffMember(StaffMember staffMember)
+        {
+            await _dbContext.StaffMembers.AddAsync(staffMember);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<StaffMember?> GetStaffMemberByEmail(string email)
+        {
+            return await _dbContext.StaffMembers.FirstOrDefaultAsync(s => s.Email == email);
+        }
+
+        public async Task<StaffMember?> GetStaffMemberByEmailorPhone(string email, string phone)
+        {
+            Expression<Func<StaffMember, bool>> FilterByKey(string key1, string key2)
+            {
+                return x => x.Email.ToLower() == key1.ToLower() || x.Phone == key2;
+            }
+            var member = await _dbContext.StaffMembers.Where(FilterByKey(email, phone)).ToListAsync();
+            return member.FirstOrDefault();
+        }
+    }
+}
---
+++ ABC.studentManagement.API/Repositories/StudentRepository.cs
@@ -0,0 +1,65 @@
+﻿using ABC.studentManagement.API.Data;
+using ABC.studentManagement.API.IRepositories;
+using ABC.studentManagement.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ABC.studentManagement.API.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StudentRepository(ApplicationDbContext context) 
+        {
+            this._dbContext = context;
+        }
+
+        public async Task<int> Add(Student student)
+        {
+            await _dbContext.Students.AddAsync(student);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<IEnumerable<Student>> GetAll()
+        {
+            return await _dbContext.Students.ToListAsync();
+        }
+
+        public async Task<Student?> Get(Guid id)
+        {
+           return await _dbContext.Students.FindAsync(id);
+        }
+
+        public async Task<int> Update(Student student)
+        {
+            _dbContext.Update(student);
+            return await _dbContext.SaveChangesAsync();
+        }
+        public async Task<int> Delete(Student student)
+        {
+            _dbContext.Students.Remove(student);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Student?> GetByEmail(string email)
+        {
+            Expression<Func<Student, bool>> FilterByKey(string key)
+            {
+                return x => x.Email.ToLower() == key.ToLower();
+            }
+            var student = await _dbContext.Students.Where(FilterByKey(email)).ToListAsync();
+            return student.FirstOrDefault();
+        }
+
+        public async Task<Student?> GetByIndexNumber(string indexNumber)
+        {
+            Expression<Func<Student, bool>> FilterByKey(string key)
+            {
+                return x => x.IndexNumber.ToLower() == key.ToLower();
+            }
+            var student = await _dbContext.Students.Where(FilterByKey(indexNumber)).ToListAsync();
+            return student.FirstOrDefault();
+        }
+    }
+}
